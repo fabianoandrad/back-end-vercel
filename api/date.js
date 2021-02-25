553165e2849e5f5c3ee03d51520e8063d981167c
fabianoandrad <affatecnologia@gmail.com>
2021-02-25 20:57:34 -0300

    test 45835000000037

--- api/date.js
+++ api/date.js
@@ -6,33 +6,19 @@
 module.exports = (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
-
-  req.setHeader("Access-Control-Allow-Credentials", true);
-  req.setHeader("Access-Control-Allow-Origin", "*");
+  
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
   );
-
-
-  req.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
 
-  req.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-
-  const body = req.body;
+  const {body} = req.query
 
   const value = body.name;
 
