cc2b6dfcea1fc7a94909859feb13e3ca66662194
fabianoandrad <affatecnologia@gmail.com>
2021-02-25 20:45:59 -0300

    test56783

--- api/date.js
+++ api/date.js
@@ -17,7 +17,7 @@ module.exports = (req, res) => {
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
 
-  const value = req.body;
+  const body = req.body;
 
   const value = body.name;
 
