5d3554fb605995ac955c43ac08ab9acabff8fe63
fabianoandrad <affatecnologia@gmail.com>
2021-02-26 17:36:38 -0300

    teste22121212

--- api/date.js
+++ api/date.js
@@ -1,7 +1,4 @@
-// module.exports = (req, res) => {
-//   const date = new Date().toString();
-//   res.status(200).send(date);
-// };
+
 const express = require('express');
 const app = express();
 const cors = require('cors')
@@ -10,13 +7,6 @@ app.use(cors())
 
 app.get("/api/date", (req, res) => {
 
-  res.json({
-    body: req.body,
-    //query: req.query,
-    //cookies: req.cookies,
-  })
-
-  const value = body.date
 
   const dateHours = {
     valueHourDiurno: 12,
@@ -26,63 +16,18 @@ app.get("/api/date", (req, res) => {
     valueMinNoturno: 12,
     NoturnoResult: "Noturno"
   }
-  console.log("passei pelo GET")
-  //res.status(200).send(dateHours);
+  
+  res.status(200).send(dateHours);
 })
 
 
-// app.post("/api/date", (req, res)=>{
+app.post("/api/date", (req, res)=>{
 
-// const values = []
-//   values = req.body;
-// console.log("passei pelo POST")
+const values = []
+  values = req.body;
   
-// return res.json(values)
-//   //res.status(200).send(values)
-// })
+  res.status(200).send(values)
+})
 
  module.exports = app
 
-
-
-// const allowCors = fn => async (req, res) => {
-//   res.setHeader('Access-Control-Allow-Credentials', true)
-//   res.setHeader('Access-Control-Allow-Origin', '*')
-//   // another common pattern
-//   res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//   )
-//   if (req.method === 'OPTIONS') {
-//     res.status(200).end()
-//     return
-//   }
-//   return await fn(req, res)
-// }
-
-// const handler = (req, res) => {
-//   res.setHeader('Access-Control-Allow-Credentials', true)
-//   res.setHeader('Access-Control-Allow-Origin', '*')
-//   res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-//   res.json({
-//         body: req.body,
-//         query: req.query,
-//         cookies: req.cookies,
-//       })
-      
-//       const dateHours = [{
-    
-//         valueHourDiurno: body,
-//         valueMinDiurno: query,
-//         DiurnoResult: 'Diurno',
-//         valueHourNoturno: cookies,
-//         valueMinNoturno: 05,
-//         NoturnoResult: 'Noturno'
-//       }]
-//       res.status(200).send(dateHours);
-// }
-
-// module.exports = allowCors(handler)
