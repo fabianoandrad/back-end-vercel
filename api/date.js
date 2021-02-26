507be4802c9707246ddaa89589793c9bd067061b
fabianoandrad <affatecnologia@gmail.com>
2021-02-26 12:09:01 -0300

    test59794

--- api/date.js
+++ api/date.js
@@ -56,6 +56,8 @@ const allowCors = fn => async (req, res) => {
 const handler = (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.json({
         body: req.body,
         query: req.query,
