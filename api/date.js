a5d9d7e401dbae6b4ea607716e93206f3a8ff214
fabianoandrad <affatecnologia@gmail.com>
2021-02-26 12:33:08 -0300

    testu937397

--- api/date.js
+++ api/date.js
@@ -11,7 +11,7 @@ module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
   // another common pattern
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  //res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
