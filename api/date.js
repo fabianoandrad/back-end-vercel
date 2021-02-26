0361acf40ffce8d747e90b3c079a3da2f1e9bbc6
fabianoandrad <affatecnologia@gmail.com>
2021-02-25 22:32:51 -0300

    test39349549777

--- api/date.js
+++ api/date.js
@@ -20,7 +20,7 @@ module.exports = (req, res) => {
   res.json({
     //body: req.body,
     query: req.query,
-    cookies: req.cookies,
+    //cookies: req.cookies,
   })
   
   const dateHours = [{
