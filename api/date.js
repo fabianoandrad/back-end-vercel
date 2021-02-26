4f3f095dc7af6cfe56f0094788518c8b6308e1bb
fabianoandrad <affatecnologia@gmail.com>
2021-02-25 22:23:42 -0300

    test83497

--- api/date.js
+++ api/date.js
@@ -15,7 +15,7 @@ module.exports = (req, res) => {
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   )
 
-  const {hourIn, minIn, hourOut, minOut, del} = req.query
+  const {hourIn, minIn, hourOut, minOut, del} = req.body
   
   const dateHours = [{
 
