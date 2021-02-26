02ff9854933934b6ed96db2fbd36b5107b1936e5
fabianoandrad <affatecnologia@gmail.com>
2021-02-26 14:20:41 -0300

    teste post334934893

--- api/date.js
+++ api/date.js
@@ -8,30 +8,20 @@ const cors = require('cors')
 
 app.use(cors())
 
-app.get("/api/date", (req, res) => {
-  // res.setHeader('Access-Control-Allow-Credentials', true)
-  // res.setHeader('Access-Control-Allow-Origin', '*')
-  // // another common pattern
-  // // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  // res.setHeader(
-  //   'Access-Control-Allow-Headers',
-  //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  // )
+// app.get("/api/date", (req, res) => {
 
-  
 
-  const dateHours = [{
-    valueHourDiurno: 01,
-    valueMinDiurno: 02,
-    DiurnoResult: 03,
-    valueHourNoturno: 04,
-    valueMinNoturno: 05,
-    NoturnoResult: 03
-  }]
+//   const dateHours = [{
+//     valueHourDiurno: 01,
+//     valueMinDiurno: 02,
+//     DiurnoResult: 03,
+//     valueHourNoturno: 04,
+//     valueMinNoturno: 05,
+//     NoturnoResult: 03
+//   }]
 
-  res.status(200).send(dateHours);
-})
+//   res.status(200).send(dateHours);
+// })
 
 
 app.post("/api/date", (req, res)=>{
