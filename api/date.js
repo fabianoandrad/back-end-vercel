f7651f210f383946bff758aab3dbfe9565907a14
fabianoandrad <affatecnologia@gmail.com>
2021-02-26 12:24:48 -0300

    test987776

--- api/date.js
+++ api/date.js
@@ -3,8 +3,43 @@
 //   res.status(200).send(date);
 // };
 
+const cors = require('cors');
 
-// module.exports = (req, res) => {
+
+module.exports = (req, res) => {
+  cors()
+  // res.setHeader('Access-Control-Allow-Credentials', true)
+  // res.setHeader('Access-Control-Allow-Origin', '*')
+  // // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  // res.setHeader(
+  //   'Access-Control-Allow-Headers',
+  //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  // )
+
+  //const {hourIn, minIn, hourOut, minOut, del} = req.body
+
+  res.json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+  })
+
+  const dateHours = [{
+
+    valueHourDiurno: body,
+    valueMinDiurno: query,
+    DiurnoResult: 'Diurno',
+    valueHourNoturno: cookies,
+    valueMinNoturno: 05,
+    NoturnoResult: 'Noturno'
+  }]
+  res.status(200).send(dateHours);
+};
+
+
+// const allowCors = fn => async (req, res) => {
 //   res.setHeader('Access-Control-Allow-Credentials', true)
 //   res.setHeader('Access-Control-Allow-Origin', '*')
 //   // another common pattern
@@ -14,66 +49,34 @@
 //     'Access-Control-Allow-Headers',
 //     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
 //   )
+//   if (req.method === 'OPTIONS') {
+//     res.status(200).end()
+//     return
+//   }
+//   return await fn(req, res)
+// }
 
-//   //const {hourIn, minIn, hourOut, minOut, del} = req.body
-
+// const handler = (req, res) => {
+//   res.setHeader('Access-Control-Allow-Credentials', true)
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
 //   res.json({
-//     body: req.body,
-//     query: req.query,
-//     cookies: req.cookies,
-//   })
-  
-//   const dateHours = [{
+//         body: req.body,
+//         query: req.query,
+//         cookies: req.cookies,
+//       })
 
-//     valueHourDiurno: body,
-//     valueMinDiurno: query,
-//     DiurnoResult: 'Diurno',
-//     valueHourNoturno: cookies,
-//     valueMinNoturno: 05,
-//     NoturnoResult: 'Noturno'
-//   }]
-//   res.status(200).send(dateHours);
-// };
+//       const dateHours = [{
 
+//         valueHourDiurno: body,
+//         valueMinDiurno: query,
+//         DiurnoResult: 'Diurno',
+//         valueHourNoturno: cookies,
+//         valueMinNoturno: 05,
+//         NoturnoResult: 'Noturno'
+//       }]
+//       res.status(200).send(dateHours);
+// }
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.json({
-        body: req.body,
-        query: req.query,
-        cookies: req.cookies,
-      })
-      
-      const dateHours = [{
-    
-        valueHourDiurno: body,
-        valueMinDiurno: query,
-        DiurnoResult: 'Diurno',
-        valueHourNoturno: cookies,
-        valueMinNoturno: 05,
-        NoturnoResult: 'Noturno'
-      }]
-      res.status(200).send(dateHours);
-}
-
-module.exports = allowCors(handler)
+// module.exports = allowCors(handler)
