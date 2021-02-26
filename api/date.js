7d6f0fc746dab144cc06c0253285537753c42690
fabianoandrad <affatecnologia@gmail.com>
2021-02-26 17:49:25 -0300

    teste post 0011

--- api/date.js
+++ api/date.js
@@ -27,12 +27,12 @@ const values = []
  // values = req.body;
 
  const dateHours = [{
-  valueHourDiurno: 12,
-  valueMinDiurno: 12,
-  DiurnoResult: "Diurno",
-  valueHourNoturno: 12,
-  valueMinNoturno: 12,
-  NoturnoResult: "Noturno"
+  valueHourDiurno: 22,
+  valueMinDiurno: 33,
+  DiurnoResult: "Diurno ok",
+  valueHourNoturno: 44,
+  valueMinNoturno: 55,
+  NoturnoResult: "Noturno ok"
 }]
   
 //return res.json(values)
