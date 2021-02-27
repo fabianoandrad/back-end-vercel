9ae59917f67009d035a6cb15bb1a6120b954a37a
fabianoandrad <affatecnologia@gmail.com>
2021-02-27 16:55:44 -0300

    teste 8484848477

--- api/date.js
+++ api/date.js
@@ -1,48 +1,45 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
 app.get("/api/date", (req, res) => {
+  const dateHours = [
+    {
+      valueHourDiurno: 12,
+      valueMinDiurno: 12,
+      DiurnoResult: "Diurno",
+      valueHourNoturno: 12,
+      valueMinNoturno: 12,
+      NoturnoResult: "Noturno",
+    },
+  ];
 
-
-  const dateHours = [{
-    valueHourDiurno: 12,
-    valueMinDiurno: 12,
-    DiurnoResult: "Diurno",
-    valueHourNoturno: 12,
-    valueMinNoturno: 12,
-    NoturnoResult: "Noturno"
-  }]
- 
   res.status(200).send(dateHours);
-})
+});
 
-
-app.post("/api/date", (req, res)=>{
+app.post("/api/date", (req, res) => {
 
   try {
-   
-const { hourIn, minIn, hourOut, minOut } = req.body;
+    const { hourIn, minIn, hourOut, minOut } = req.body;
+    const dateHours = [];
 
- const dateHours = [{
-  valueHourDiurno: hourIn,
-  valueMinDiurno: minIn,
-  DiurnoResult: "Diurno ok",
-  valueHourNoturno: hourOut,
-  valueMinNoturno: minOut,
-  NoturnoResult: "Noturno ok"
-}]
-  
+    dateHours = {
+      valueHourDiurno: hourIn,
+      valueMinDiurno: minIn,
+      DiurnoResult: "Diurno ok",
+      valueHourNoturno: hourOut,
+      valueMinNoturno: minOut,
+      NoturnoResult: "Noturno ok",
+    };
 
-  res.status(200).send(dateHours)
-    
+    res.status(200).send(dateHours);
+
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
+  
+});
 
-})
-
- module.exports = app
+module.exports = app;
