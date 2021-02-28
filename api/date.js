c9aacfb497053b392e0eb7d2592b56ad60894ca4
fabianoandrad <affatecnologia@gmail.com>
2021-02-28 20:36:50 -0300

    Test calculo

--- api/date.js
+++ api/date.js
@@ -11,36 +11,168 @@ app.use(cors());
 let dateHours = []
 
 app.get("/api/date", (req, res) => {
-  // dateHour =  [{
-  //     valueHourDiurno: 12,
-  //     valueMinDiurno: 12,
-  //     DiurnoResult: "Diurno",
-  //     valueHourNoturno: 12,
-  //     valueMinNoturno: 12,
-  //     NoturnoResult: "Noturno",
-  //   }],
-  
 
   res.status(200).send(dateHours);
+  
 });
 
 app.post("/api/date", (req, res) => {
 
   try {
 
-    //const valuesDate = req.body
+    const { hourIn, minIn, hourOut, minOut, del } = req.body;
 
-    const { hourIn, minIn, hourOut, minOut } = req.body;
-
-    let valueHourDiurno = hourIn
-    let valueMinDiurno = minIn
-    let valueHourNoturno = hourOut
-    let valueMinNoturno = minOut
-    let DiurnoResult = "Diurno"
-    let NoturnoResult = "Noturno"
+    let valueHourIn = parseInt(hourIn);
+    let valueMinIn = parseInt(minIn);
+    let valueHourOut = parseInt(hourOut);
+    let valueMinOut = parseInt(minOut);
+    if (valueHourOut == 00) valueHourOut = 24;
+    let valueHourDiurno = 0;
+    let valueMinDiurno = 0;
+    let valueHourNoturno = 0;
+    let valueMinNoturno = 0;
+    let DiurnoResult = "Não fez horas diurnas!";
+    let NoturnoResult = "Não fez horas noturnas!";
+    let totalHours = 0;
+    let totalMin = 0;
   
-
-    const valuesDate = {
+    // se entrada for diurna
+    if (valueHourIn >= 5 && valueHourIn < 22) {
+      //executa se saida é menor que 22hs
+      if (valueHourOut >= 5 && valueHourOut < 22 && valueHourOut > valueHourIn) {
+        //executa se minOut for < minIn
+        if (valueMinOut < valueMinIn) {
+          valueMinOut += 60;
+          valueHourOut -= 1;
+  
+          valueMinDiurno = valueMinOut - valueMinIn;
+          valueHourDiurno = valueHourOut - valueHourIn;
+          DiurnoResult = "de horas diurnas";
+        } else {
+          valueMinDiurno = valueMinOut - valueMinIn;
+          valueHourDiurno = valueHourOut - valueHourIn;
+          DiurnoResult = "de horas diurnas";
+        }
+  
+        //executa se saida é maior ou igual a 22hs
+      } else {
+        //resultado para hora diurna até as 22hrs
+        if (valueMinIn == 0) {
+          valueMinDiurno = 00 - valueMinIn;
+          valueHourDiurno = 22 - valueHourIn;
+          DiurnoResult = "de horas diurnas";
+        } else {
+          valueMinIn -= 60;
+          valueHourIn += 1;
+  
+          valueMinDiurno = 00 - valueMinIn;
+          valueHourDiurno = 22 - valueHourIn;
+          DiurnoResult = "de horas diurnas";
+        }
+  
+        //executa quando hora de saida for entre 22hrs e 24hrs
+        if (valueHourOut >= 22 && valueHourOut < 24) {
+          //resultado para hora noturna
+          valueMinNoturno = valueMinOut - 0;
+          valueHourNoturno = valueHourOut - 22;
+          NoturnoResult = "de horas noturnas";
+        } else {
+          if (valueHourOut == 24) valueHourOut = 00;
+          //se horario de saida passou das 00hr
+          if (valueHourOut < 5) {
+            valueMinNoturno = valueMinOut;
+            valueHourNoturno = valueHourOut + 2;
+            NoturnoResult = "de horas noturnas";
+          } else {
+            // calcular horas após as 5hs
+            valueHourOut -= 5;
+  
+            valueMinDiurno += valueMinOut;
+            valueHourDiurno += valueHourOut;
+  
+            if (valueMinDiurno >= 60) {
+              valueMinDiurno -= 60;
+              valueHourDiurno += 1;
+  
+            }
+  
+            valueMinNoturno = 00;
+            valueHourNoturno = 07;
+            NoturnoResult = "de horas noturnas";
+          }
+        }
+      }
+    }
+  
+    // ***************************** Se entrada for noturna *****************************
+    if (valueHourIn >= 22 || valueHourIn < 5) {
+      if (valueHourOut >= 22 && valueHourOut <= 24) {
+        if (valueMinIn > valueMinOut) {
+          valueMinOut += 60;
+          valueHourOut -= 1;
+  
+          valueMinNoturno = valueMinOut - valueMinIn;
+          valueHourNoturno = valueHourOut - valueHourIn;
+          NoturnoResult = "de horas noturnas";
+        } else {
+          valueMinNoturno = valueMinOut - valueMinIn;
+          valueHourNoturno = valueHourOut - valueHourIn;
+          NoturnoResult = "de horas noturnas";
+          //console.log("Passei aqui");
+        }
+      } else {
+        //*********************Teste *****************
+  
+        if (valueHourOut == 24) valueHourOut = 00;
+  
+        if (valueMinIn == 0) {
+          valueMinNoturno = 00 - valueMinIn;
+          valueHourNoturno = 24 - valueHourIn;
+        } else {
+          valueMinIn -= 60;
+          valueHourIn += 1;
+  
+          valueMinNoturno = 00 - valueMinIn;
+          valueHourNoturno = 24 - valueHourIn;
+        }
+  
+        // se caso entrada for entre 22hrs 00 e saida entre 00hr e 5hrs  
+        if (valueHourIn >= 22 && valueHourOut > 00  && valueHourOut <= 5) {
+  
+  
+          // somando horas entre 22hrs 00 e saida entre 00hr e 5hrs
+          valueMinNoturno += valueMinOut
+          valueHourNoturno += valueHourOut
+  
+          if(valueMinNoturno >= 60){
+            valueMinNoturno -= 60
+            valueHourNoturno += 1
+          }
+          NoturnoResult = "de horas noturnas";
+  
+  
+        } else {
+          // calcular horas após as 5hs
+          valueHourNoturno += 5
+  
+          valueHourOut -= 5;
+  
+          valueMinDiurno += valueMinOut;
+          valueHourDiurno += valueHourOut;
+  
+          if (valueMinDiurno >= 60) {
+            valueMinDiurno -= 60;
+            valueHourDiurno += 1;
+          }
+          NoturnoResult = "de horas noturnas";
+          DiurnoResult = "de horas diurnas";
+  
+        }
+      }
+    }
+  
+    const project = {
+      id: uuid(),
       valueHourDiurno,
       valueMinDiurno,
       DiurnoResult,
@@ -48,10 +180,15 @@ app.post("/api/date", (req, res) => {
       valueMinNoturno,
       NoturnoResult,
     };
-
-   dateHours.push(valuesDate)
-
-    res.status(200).send(valuesDate);
+  
+    projects.push(project);
+  
+    if(del === true) {
+    
+      projects = [];
+     }
+  
+    return res.json(project);
 
   } catch (error) {
     res.send(error.message);
