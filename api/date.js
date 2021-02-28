b65828853d27e80bd338bec73633916308e4ba26
fabianoandrad <affatecnologia@gmail.com>
2021-02-28 19:54:47 -0300

    teste heroku

--- api/date.js
+++ api/date.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors("https://create-react-app-theta-beryl.vercel.app"));
 
 let dateHours = []
 
