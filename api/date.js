144bafc3746a85207cf7d06ca0450ecbdf929648
fabianoandrad <affatecnologia@gmail.com>
2021-02-28 20:17:38 -0300

    teste body parse

--- api/date.js
+++ api/date.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cors());
 
