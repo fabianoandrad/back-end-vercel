36246d1f3e3716021c01e605444e6b695c51069a
fabianoandrad <affatecnologia@gmail.com>
2021-03-01 14:16:05 -0300

    teste dele rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,31 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+// import svelte from 'rollup-plugin-svelte';
+// import resolve from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+// import livereload from 'rollup-plugin-livereload';
+// import { terser } from 'rollup-plugin-terser';
 
-const production = !process.env.ROLLUP_WATCH;
+// const production = !process.env.ROLLUP_WATCH;
 
-function serve() {
-	let server;
+// function serve() {
+// 	let server;
 	
-	function toExit() {
-		if (server) server.kill(0);
-	}
+// 	function toExit() {
+// 		if (server) server.kill(0);
+// 	}
 
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
+// 	return {
+// 		writeBundle() {
+// 			if (server) return;
+// 			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+// 				stdio: ['ignore', 'inherit', 'inherit'],
+// 				shell: true
+// 			});
 
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
-}
+// 			process.on('SIGTERM', toExit);
+// 			process.on('exit', toExit);
+// 		}
+// 	};
+// }
 
 // export default {
 // 	input: 'src/main.js',
