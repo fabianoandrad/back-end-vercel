3836fee735855db198a934ce91521a2f1d163c81
fabianoandrad <affatecnologia@gmail.com>
2021-03-01 14:44:26 -0300

    test concatenar

--- api/date.js
+++ api/date.js
@@ -172,7 +172,7 @@ app.post("/api/date", (req, res) => {
       }
     }
 
-    if (valueMinDiurno < 10) valueMinDiurno = "0" + toString(valueMinDiurno)
+    if (valueMinDiurno < 10) valueMinDiurno = "0" + valueMinDiurno
 
       const valueHourMin = {
         id: uuid(),
